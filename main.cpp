72beecc4ec4d0948320caca5a8f79e516846962b
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2021-12-10 20:23:43 +0330

    Write the first power function

--- main.cpp
+++ main.cpp
@@ -4,6 +4,18 @@
 using namespace std;
 using namespace std::chrono;
 
+long power1(long x, long  y) // The first power function
+{
+    long k = 1;
+    int count = 0; // To count the number of multiplication operations
+    for (long n = 1; n <= y; n++)
+    {
+        count += 1; // Add a multiplication operation
+        k *= x;
+    }
+    return count; // Returns the number of multipled operations
+}                 // end function 1
+
 int main()
 {
 
